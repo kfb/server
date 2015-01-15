4f1e31c2cc75972663d0c8afa89ca714af93102a
kfb <kaleidoscopicfilmstripblues@gmail.com>
2015-01-15 12:45:31 +0100

    Add minimal HTTP server implementation

---
+++ server.py
@@ -0,0 +1,21 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+ 
+class Handler(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        
+    def do_POST(self):
+        self._set_headers()
+        
+        print self.headers
+        print self.rfile.read(self.headers["Content-Length"])
+        
+def main():
+    httpd = HTTPServer(('', 8080), Handler)
+    httpd.serve_forever()
+ 
+if __name__ == "__main__":
+    main()
