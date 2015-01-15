e861e7aee6e702f5adc8bbd53fa449881fed9416
kfb <kaleidoscopicfilmstripblues@gmail.com>
2015-01-15 20:40:24 +0100

    Huge refactoring

--- server.py
+++ server.py
@@ -1,12 +1,151 @@
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import SocketServer
+import re
+import json
+import getpass
+import sys
 
-class PullRequestHandler:
-    def __init__(self):
+from agithub import Github
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+class GithubEventHandler:
+    def __init__(self, username, password, repo):
+        # The basic pattern splits the input into a "verb + data" group;
+        # individual handling operations might split the data further.
+        self.pattern = re.compile("^(\w+) (.*)$")
+
+        self.username = username
+        self.password = password
+        self.repo = repo
+
+        self.github = Github(username, password)
+
+        # A partial to be used in requests
+        self.git_data = self.github.repos[self.username][self.repo].git
+
+class IssueEventHandler(GithubEventHandler):
+    def handle(self, event):
+        # Parse the incoming event
+        event = json.loads(event)
+
+        # We're interested in tickets being opened
+        if event["action"] == "opened":
+            title = event["issue"]["title"]
+            match = self.pattern.match(title)
+
+            operations = {
+                "add": self.add_file,
+                "del": self.remove_file,
+                "update": self.update_file,
+            }
+
+            if match and match.group(1) in operations:
+                operations[match.group(1)](match.group(2))
+            else:
+                print "Couldn't understand title: '%'" % title
+
+    def add_file(self, data):
+        print "Adding file (data = '%s')" % data
+        
+        # Obtain the current HEAD
+        head_sha = _get_head_commit()
+
+        # Create a new blob
+        blob_sha = _create_blob("New file created by IssueEventHandler")
+
+        # Create a new tree
+        tree_sha = _create_tree(head_sha, data, blob_sha)
+
+        # Create a commit
+        commit_sha = _create_commit(tree_sha, [head_sha])
+
+        # Update refs/heads/master
+        if _update_refs_heads_master(commit_sha):
+            print "Successfully created %s in %s" % (data, self.repo)
+
+    def remove_file(self, data):
         pass
 
-    def handle(event):
-        print event
+    def update_file(self, data):
+        pass
+
+    def _update_refs_heads_master(self, sha):
+        response = _patch_request(self.git_data.refs.heads.master,
+            {
+                "sha": sha
+            })
+
+        return response
+
+    def _create_commit(self, path, tree, parents):
+        response = _post_request(self.git_data.commits,
+            {
+                "message": "Add %s" % path,
+                "tree": tree,
+                "parents": parents
+            })
+
+        return response["sha"]
+
+    def _create_tree(self, base_tree, path, sha):
+        response = _post_request(self.git_data.trees,
+            {
+                "base_tree": base_tree, 
+                "tree": [{
+                    "path": path,
+                    "mode": "100644",
+                    "type": "blob",
+                    "sha": sha
+                }]
+            })
+
+        return response["sha"]
+
+    def _create_blob(self, content):
+        response = _post_request(self.git_data.blobs,
+            {
+                "content": content,
+                "encoding": "utf-8"
+            })
+
+        return response["sha"]
+
+    def _get_head_commit(self):
+        response = _get_request(self.git_data.refs.heads.master)
+
+        return response["object"]["sha"]
+
+    def _get_request(self, endpoint):
+        status, response = endpoint.get()
+
+        if status == 200:
+            return response
+        else:
+            print "_get_request: failed (%d)" % status
+            print "_get_request: response = \n%s", response
+
+            return None
+
+    def _post_request(self, endpoint, payload):
+        status, response = endpoint.post(body=payload)
+
+        if status == 200:
+            return response
+        else:
+            print "_post_request: failed (%d)" % status
+            print "_post_request: response = \n%s", response
+
+            return None
+
+    def _patch_request(self, endpoint, payload):
+        status, response = endpoint.patch(body=payload)
+
+        if status == 200:
+            return response
+        else:
+            print "_post_request: failed (%d)" % status
+            print "_post_request: response = \n%s", response
+
+            return None
  
 class Handler(BaseHTTPRequestHandler):
     def _set_headers(self):
@@ -17,12 +156,21 @@ class Handler(BaseHTTPRequestHandler):
     def do_POST(self):
         self._set_headers()
 
-        if self.headers["X-GitHub-Event"] == "pull_request":
-            PullRequestHandler().handle(self.rfile.read(int(self.headers["Content-Length"])))
-        
+        if self.headers["X-GitHub-Event"] == "issues":
+            self.server.issues_handler.handle(self.rfile.read(int(self.headers["Content-Length"])))
+
+class Server(HTTPServer):
+    def __init__(self, server_address, RequestHandlerClass, username, password, repo):
+        HTTPServer.__init__(self, server_address, RequestHandlerClass)
+
+        # Only issues for now, but this can be extended in the future
+        self.issues_handler = IssueEventHandler(username, password, repo)
+
 if __name__ == "__main__":
     try:
-        httpd = HTTPServer(('', 8080), Handler)
+        password = getpass.getpass()
+
+        httpd = Server(('', 8080), Handler, sys.argv[1], password, sys.argv[2])
         httpd.serve_forever()
     except KeyboardInterrupt:
         httpd.socket.close()
